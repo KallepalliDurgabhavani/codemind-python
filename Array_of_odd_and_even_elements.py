5d0e309f6f411e38b6f98eda84d7d46a5b074f6e
KallepalliDurgabhavani <119723747+KallepalliDurgabhavani@users.noreply.github.com>
2023-05-24 08:28:49 +0530

    n=int(input()) l=list(map(int,input().split())) o=list() e=list() for i in range(0,n):     if l[i]%2==0:         e.append(l[i])     else:         o.append(l[i]) s=o+e for i in range(0,n):     print(s[i],end=' ')

--- Array_of_odd_and_even_elements.py
+++ Array_of_odd_and_even_elements.py
@@ -1,13 +1,12 @@
 n=int(input())
-e=list()
-o=list()
 l=list(map(int,input().split()))
+o=list()
+e=list()
 for i in range(0,n):
     if l[i]%2==0:
         e.append(l[i])
     else:
         o.append(l[i])
-s=list(o+e)
-for j in s:
-    print(j,end=' ')
-        
+s=o+e
+for i in range(0,n):
+    print(s[i],end=' ')
