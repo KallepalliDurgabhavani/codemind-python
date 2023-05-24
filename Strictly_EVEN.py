d2c56bd3b2b278c4b1ee131fa30620525bf51458
KallepalliDurgabhavani <119723747+KallepalliDurgabhavani@users.noreply.github.com>
2023-05-24 07:19:32 +0530

    n=int(input()) t=0 l=list(map(int,input().split())) for i in range(0,n):     if i%2==0 and l[i]%2!=0:         t=1;         break if t==0:     print(True) else:     print(False)

---
+++ Strictly_EVEN.py
@@ -0,0 +1,11 @@
+n=int(input())
+t=0
+l=list(map(int,input().split()))
+for i in range(0,n):
+    if i%2==0 and l[i]%2!=0:
+        t=1;
+        break
+if t==0:
+    print(True)
+else:
+    print(False)
