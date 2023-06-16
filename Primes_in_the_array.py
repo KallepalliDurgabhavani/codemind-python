9758667a8c1a8198faee3653598f4dcc28447f60
KallepalliDurgabhavani <119723747+KallepalliDurgabhavani@users.noreply.github.com>
2023-06-16 18:32:17 +0530

    def prime(n):     s=0     for i in range(1,n+1):         if n%i==0:             s+=1     if s==2:         return 1     else:         return 0 n=int(input()) l=list(map(int,input().split())) c=0 for i in l:     if prime(i)==1:         c+=1 print(c)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,16 @@
+def prime(n):
+    s=0
+    for i in range(1,n+1):
+        if n%i==0:
+            s+=1
+    if s==2:
+        return 1
+    else:
+        return 0
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+for i in l:
+    if prime(i)==1:
+        c+=1
+print(c)
